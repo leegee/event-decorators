eed5a2cacd5906b537f6268bbb8b5e59499513b7
Lee Goddard <leegee@gmail.com>
2018-10-05 11:15:32 +0200

    Listener

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
-export function Listen (Class) {
-    return (...args) => {
-        console.log(args);
-        return new Class(args);
+// export function Listen(arg: string) {
+//     function f(target) {
+//         function ff(msg: string) {
+//             return new target(arg + ":" + msg)
+//         }
+//         return <typeof A><any>ff
+//     }
+//     return f
+// }
+
+export function Listen(eventName: string) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        console.log('called ',eventName, target, propertyKey, descriptor    );
+        document.addEventListener(eventName, descriptor.value);
+        return descriptor;
     };
 }
--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,35 @@
 import 'mocha';
 import { expect } from 'chai';
-
 import * as Testing from '../src/';
+import { Listen } from '../src/';
+
+let spyRan = false;
+const eventName = 'test-event-name';
+
+class TestSubject {
+    @Listen(eventName)
+    listeningMember(){
+        console.log('**************** listening member called');
+        spyRan = true;
+    }
+}
 
 describe('Init', () => {
     it('loads and exports', () => {
         expect(Testing.Listen).to.be.a('function');
+    });
+});
+
+describe('Listen', () => {
+    const testSubjectFunc = () => {
+        console.log('enter test subject member');
+    }
+
+    it('receives an event', () => {
+        expect(spyRan).to.be.false;
+        const event = new CustomEvent(eventName);
+        const notCancelled = document.dispatchEvent(event);
+        expect(notCancelled).to.be.true;
+        expect(spyRan).to.be.true;
     });
 });
